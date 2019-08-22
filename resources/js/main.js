e1b470dbb90808063462873d7909a4939a22386e
Zara Nunn <ZaraNunn@zaras-imac.home>
2019-08-22 17:51:08 +0100

    Readded js folder.

---
+++ resources/js/main.js
@@ -0,0 +1,61 @@
+
+
+
+$(document).ready(function() {
+
+
+
+  $('.carousel').slick({
+    slidestoShow: 1,
+    slidestoScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    draggable: false,
+    pauseonFocus: false,
+    pauseonHover: false,
+    infinite: true,
+    adaptiveHeight: false,
+});
+
+
+var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+    var navHeight = $('.navbar').css('height');
+    $('.navbar').animate({top: '-' + navHeight}, 150);
+    lastScrollTop = scrollTop;
+  } else if (lastScrollTop - scrollTop > 50) {
+    $('.navbar').animate({top: '0px'}, 150);
+    lastScrollTop = scrollTop;
+  }
+});
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-65px";
+  }
+  prevScrollpos = currentScrollPos;
+
+
+};
+
+
+
+
+});
+
+
+/*$(document).ready(function(){
+  $(".menu-toggle").click(function) {
+
+  $("mobile-nav").toggleClass()("site-nav--open")
+  });
+
+});*/
